c90bc35bb3ecc2d969dd2cbefce07ba24e80c852
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-20 19:05:18 +0300

    Updating the form to use formspree

--- src/modules/ui/LeadCapture.js
+++ src/modules/ui/LeadCapture.js
@@ -1,36 +1,46 @@
 /** @jsx jsx */
 import {jsx, Label, Input, Box, Button} from 'theme-ui';
+import React from 'react';
+import {useForm, ValidationError} from '@formspree/react';
 
-const LeadCapture = ({children, ...otherProps}) => (
-  <Box
-    as="form"
-    sx={{
-      px: '24px',
-      py: '28px',
-      mb: '24px',
-      mt: '5',
-      width: '85%',
-      borderRadius: '12px',
-      bg: 'primaryMuted'
-    }}
-    onSubmit={(e) => e.preventDefault()}>
-    <b>Would you like to know more about our enterprise packages?</b>
-    <Label htmlFor="email">Leave your email and we'll be in touch</Label>
-    <Input
-      name="email"
-      id="email"
+const LeadCapture = ({children, ...otherProps}) => {
+  const [state, handleSubmit] = useForm('mjvlgalg');
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+
+  return (
+    <Box
+      as="form"
       sx={{
-        width: '70%',
-        mb: '3',
-        mr: '3',
-        borderColor: 'secondaryMuted',
-        display: 'inline-block',
-        bg: 'white'
+        px: '24px',
+        py: '28px',
+        mb: '24px',
+        mt: '5',
+        width: '85%',
+        borderRadius: '12px',
+        bg: 'primaryMuted'
       }}
-    />
-
-    <Button>Submit</Button>
-  </Box>
-);
+      onSubmit={handleSubmit}>
+      <b>Would you like to know more about our enterprise packages?</b>
+      <Label htmlFor="email">Leave your email and we'll be in touch</Label>
+      <Input
+        name="email"
+        id="email"
+        type="email"
+        sx={{
+          width: '70%',
+          mb: '3',
+          mr: '3',
+          borderColor: 'secondaryMuted',
+          display: 'inline-block',
+          bg: 'white'
+        }}
+      />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <Button>Submit</Button>
+    </Box>
+  );
+};
 
 export default LeadCapture;
