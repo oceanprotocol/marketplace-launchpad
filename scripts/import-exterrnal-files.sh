bf57b08433fc611c7a3caeb7d2ce04fe63edef91
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-20 15:16:58 +0300

    Updating colours and script

---
+++ scripts/import-exterrnal-files.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/oceanprotocol/docs/main/content/tutorials/wallets.md -O content/en/learn/level-1/wallets.md
+wget https://raw.githubusercontent.com/oceanprotocol/docs/main/content/tutorials/metamask-setup.md -O content/en/learn/level-1/metamask-setup.md
