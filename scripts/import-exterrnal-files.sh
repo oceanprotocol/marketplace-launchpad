1cb268320eb1ac150e2cae22597078a62932ed2c
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-20 15:45:52 +0300

    Downloading market publishing inscruction guide

--- scripts/import-exterrnal-files.sh
+++ scripts/import-exterrnal-files.sh
@@ -1,2 +1,3 @@
 wget https://raw.githubusercontent.com/oceanprotocol/docs/main/content/tutorials/wallets.md -O content/en/learn/level-1/wallets.md
 wget https://raw.githubusercontent.com/oceanprotocol/docs/main/content/tutorials/metamask-setup.md -O content/en/learn/level-1/metamask-setup.md
+wget https://raw.githubusercontent.com/oceanprotocol/docs/main/content/tutorials/marketplace-publish-data-asset.md -O content/en/learn/level-1/marketplace-publish-data-asset.md
