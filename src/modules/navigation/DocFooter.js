a49daee7aa7f1a47af403aab1b2f5b282408784d
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-01-26 10:41:16 +0300

    Formatting footer links

--- src/modules/navigation/DocFooter.js
+++ src/modules/navigation/DocFooter.js
@@ -3,21 +3,21 @@ import {Fragment} from 'react';
 import {jsx, Text, Flex} from 'theme-ui';
 import {Icon} from '@makerdao/dai-ui-icons';
 import {social, githubContentPath} from '../../../config';
+import {Link} from '@modules/navigation';
 
 const DocFooter = (path) => {
   // console.log("path", path)
   const url = githubContentPath + path.path;
 
   return (
-    <Fragment>
-      <a href={social.Discord} target="_blank" rel="noreferrer">
+    <div sx={{display: 'flex', justifyContent: 'space-between'}}>
+      <Link href={url} target="_blank" rel="noreferrer">
+        ✏️ Edit this page on GitHub
+      </Link>
+      <Link href={social.Discord} target="_blank" rel="noreferrer">
         ✋ Ask a question on Discord
-      </a>
-      <br />
-      <a href={url} target="_blank" rel="noreferrer">
-        ✏️Edit this page on GitHub
-      </a>
-    </Fragment>
+      </Link>
+    </div>
   );
 };
 
